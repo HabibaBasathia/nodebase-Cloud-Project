44447f396ef4d1d339db6453079df44086766794
Habiba Basathia <150885118+HabibaBasathia@users.noreply.github.com>
2026-05-17 14:25:02 +0500

    Create RDS security group with ingress and egress rules
    Added a new security group for RDS with ingress and egress rules.

--- terraform/main.tf
+++ terraform/main.tf
@@ -105,6 +105,30 @@ resource "aws_security_group" "rds_sg" {
     protocol        = "tcp"
     security_groups = [aws_security_group.app_sg.id]
   }
+  resource "aws_security_group" "rds_sg" {
+  name   = "nodebase-rds-sg"
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port       = 5432
+    to_port         = 5432
+    protocol        = "tcp"
+    security_groups = [aws_security_group.app_sg.id]
+  }
+
+  ingress {
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 # --- 3. DATABASE & STORAGE ---
